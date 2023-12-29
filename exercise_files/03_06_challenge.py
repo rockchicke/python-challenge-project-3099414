5431910aea6c27725f684a6a783cf09332bc463e
rockchicke <155102397+rockchicke@users.noreply.github.com>
2023-12-29 11:23:21 -0600

    Update 03_06_challenge.py
    My updated version of this challenge

--- exercise_files/03_06_challenge.py
+++ exercise_files/03_06_challenge.py
@@ -1,6 +1,8 @@
 import os
 import time
+import math
 from termcolor import colored
+
 
 class Canvas:
     def __init__(self, width, height):
@@ -9,10 +11,10 @@ class Canvas:
         self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
 
     def hitsWall(self, point):
-        return point[0] < 0 or point[0] >= self._x or point[1] < 0 or point[1] >= self._y
+        return round(point[0]) < 0 or round(point[0]) >= self._x or round(point[1]) < 0 or round(point[1]) >= self._y
 
     def setPos(self, pos, mark):
-        self._canvas[pos[0]][pos[1]] = mark
+        self._canvas[round(pos[0])][round(pos[1])] = mark
 
     def clear(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -22,6 +24,7 @@ class Canvas:
         for y in range(self._y):
             print(' '.join([col[y] for col in self._canvas]))
 
+
 class TerminalScribe:
     def __init__(self, canvas):
         self.canvas = canvas
@@ -29,26 +32,27 @@ class TerminalScribe:
         self.mark = '*'
         self.framerate = 0.05
         self.pos = [0, 0]
+        self.direction = [0, 1]
+
+    def setDegrees(self, degrees):
+        radians = (degrees/180) * math.pi
+        self.direction = [math.sin(radians), -math.cos(radians)]
 
     def up(self):
-        pos = [self.pos[0], self.pos[1]-1]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
+        self.direction = [0, -1]
+        self.forward()
 
     def down(self):
-        pos = [self.pos[0], self.pos[1]+1]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
+        self.direction = [0, 1]
+        self.forward()
 
     def right(self):
-        pos = [self.pos[0]+1, self.pos[1]]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
+        self.direction = [1, 0]
+        self.forward()
 
     def left(self):
-        pos = [self.pos[0]-1, self.pos[1]]
-        if not self.canvas.hitsWall(pos):
-            self.draw(pos)
+        self.direction = [-1, 0]
+        self.forward()
 
     def drawSquare(self, size):
         i = 0
@@ -68,7 +72,11 @@ class TerminalScribe:
             self.up()
             i = i + 1
 
-
+    def forward(self):
+        pos = [self.pos[0] + self.direction[0],
+               self.pos[1] + self.direction[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
 
     def draw(self, pos):
         self.canvas.setPos(self.pos, self.trail)
@@ -77,8 +85,10 @@ class TerminalScribe:
         self.canvas.print()
         time.sleep(self.framerate)
 
+
 canvas = Canvas(30, 30)
 scribe = TerminalScribe(canvas)
 
-scribe.drawSquare(20)
-
+scribe.setDegrees(135)
+for i in range(30):
+    scribe.forward()
